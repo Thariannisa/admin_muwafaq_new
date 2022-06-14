b38bfe7b5d9357cc7b16baf2ac4a181ba1a125fa
thari.annisa021@gmail.com <thari.annisa021@gmail.com>
2022-06-14 10:45:04 +0700

    update

--- admin/auth/routes.py
+++ admin/auth/routes.py
@@ -32,13 +32,16 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
 
 
 GOOGLE_CLIENT_ID = "908376936693-j3g8s0pgq0ihtid7b8806ksmlt6ac95h.apps.googleusercontent.com"
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+client_secrets_file = os.path.join(
+    pathlib.Path(__file__).parent, "client_secret.json")
 
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email", "openid"],
     redirect_uri="http://127.0.0.1:5000/auth/login_google"
 )
+
 
 def login_is_required(function):
     def wrapper(*args, **kwargs):
@@ -49,11 +52,13 @@ def login_is_required(function):
 
     return wrapper
 
+
 @auth.route("/google")
 def googlelogin():
     authorization_url, state = flow.authorization_url()
     session["state"] = state
     return redirect(authorization_url)
+
 
 @auth.route("/login_google")
 def callback():
@@ -65,7 +70,8 @@ def callback():
     credentials = flow.credentials
     request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    token_request = google.auth.transport.requests.Request(
+        session=cached_session)
 
     id_info = id_token.verify_oauth2_token(
         id_token=credentials._id_token,
@@ -76,6 +82,7 @@ def callback():
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
     return redirect("/")
+
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
@@ -190,6 +197,7 @@ def delete(id):
             return redirect('/auth/list_admin')
     else:
         return "gagal"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
